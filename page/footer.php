d03d0e59179978bade573129634209ffae236397
David-Schoots <davidschoots@live.nl>
2024-09-04 16:09:16 +0200

    mid section added

--- page/footer.php
+++ page/footer.php
@@ -1,39 +1,33 @@
-<footer style="padding: 20px 0; position: relative; background: #707070; overflow: hidden;">
+<footer class="py-4" style="background: #707070; position: relative; overflow: hidden;">
   <div class="container">
-    <div class="row" style="position:relative;">
+    <div class="row">
       <!-- 1st Column: Logo and Text -->
       <div class="col-12 col-md-5 mb-4 d-flex flex-column align-items-start">
-        <div class="row">
-          <div class="col-12">
-            <img src="../assets/logo/logo_footer.png" class="img-fluid mb-3" width="50%" alt="logo">
-          </div>
-          <div class="col-7">
-            <p class="mt-2" style="color: white;">
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-            </p>
-          </div>
-          <div class="col-12">
-            <button style="background-color: white; color: black; border: none; padding: 10px 20px; margin-top: 10px;">LEES MEER</button>
-          </div>
-        </div>
+        <img src="../assets/logo/logo_footer.png" class="img-fluid mb-3" style="max-width: 200px;" alt="logo">
+        <p class="text-white" style="max-width: 350px;">
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+        </p>
+        <button class="btn btn-light text-dark mt-2">LEES MEER</button>
       </div>
 
       <!-- 2nd Column: Navigation Links -->
-      <div class="col-12 col-md-3 mb-4 mt-5 d-flex flex-column align-items-start">
-        <h3 style="color: white;">NAVIGEER</h3>
-        <a href="#" style="color: white;" class="d-block mb-2">Werken bij</a>
-        <a href="#" style="color: white;" class="d-block mb-2">Veelgestelde vragen</a>
-        <a href="#" style="color: white;" class="d-block mb-2">Vestigingen</a>
-        <a href="#" style="color: white;" class="d-block">Contact</a>
+      <div class="col-12 col-md-3 mb-4 mt-4">
+        <h3 class="text-white">NAVIGEER</h3>
+        <ul class="list-unstyled">
+          <li><a href="#" class="text-white d-block mb-2">Werken bij</a></li>
+          <li><a href="#" class="text-white d-block mb-2">Veelgestelde vragen</a></li>
+          <li><a href="#" class="text-white d-block mb-2">Vestigingen</a></li>
+          <li><a href="#" class="text-white d-block">Contact</a></li>
+        </ul>
       </div>
 
       <!-- 3rd Column: Social Media -->
-      <div class="col-12 col-md-4 mb-4 mt-5 align-items-start">
-        <h3 style="color: white;">VOLG ONS</h3>
-        <div class="align-items-center">
-          <a href="#" title="Facebook"><img src="../assets/icons/facebook.png" width="10%" alt="Facebook"></a>
-          <a href="#" title="Twitter"><img src="../assets/icons/twitter.png" width="10%" alt="Twitter"></a>
-          <a href="#" title="Instagram"><img src="../assets/icons/instagram.png" width="10%" alt="Instagram"></a>
+      <div class="col-12 col-md-4 mb-4 mt-4">
+        <h3 class="text-white">VOLG ONS</h3>
+        <div class="d-flex">
+          <a href="#" title="Facebook" class="me-2"><img src="../assets/icons/facebook.png" style="width: 40px;" alt="Facebook"></a>
+          <a href="#" title="Twitter" class="me-2"><img src="../assets/icons/twitter.png" style="width: 40px;" alt="Twitter"></a>
+          <a href="#" title="Instagram"><img src="../assets/icons/instagram.png" style="width: 40px;" alt="Instagram"></a>
         </div>
       </div>
     </div>
--- page/index.php
+++ page/index.php
@@ -2,6 +2,32 @@
 include_once("header.php");
 ?>
 
+<!-- Introduction Section -->
+<div class="container mt-5 text-white p-4" style="background-color: #6E4F7D; max-width: 86%;">
+    <h1 class="mb-4 text-uppercase" style="font-size: 60px;">welkom bij annexbios 3</h1>
+    <p class="mb-4" style="font-size: 25px; max-width: 850px;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient</p>
+    <button class="px-4 py-2 fw-bold" style="color: #6E4F7D; border: none;">BEKIJK DE DRAAIENDE FILMS</button>
+</div>
+<!-- End of Introduction Section -->
+
+<!-- information company -->
+
+<div class="container mt-4 p-4" style="background-color: #6E4F7D; max-width: 86%;">
+        <div class="row">
+            <!-- 1st column: photo -->
+            <div class="col-12 col-md-6 mb-4">
+                <img src="../assets/maps/maps.png" alt="location" class="img-fluid" style="max-height: 300px; width: 50%;">
+            </div>
+            
+            
+    </div>
+
+   
+
+</div> <!-- End of information company -->
+
+
+
 
 <?php
 include_once("footer.php");
