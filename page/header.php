8171001a1d87efd571023980b64d4e85cc9a5623
Tjardo <tjardo.antonie@gmail.com>
2024-09-04 13:44:47 +0200

    header and css added

--- page/header.php
+++ page/header.php
@@ -6,10 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/header.css">
+
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+            <a class="navbar-brand" href="#">
+                <img class="ms-20" src="../assets/logo/bilthoven_logo.png" alt="AnnexBios" style="height: 100px;">
+            </a>
+            <ul class=" navbar-nav d-flex flex-row">
+                <li class="nav-item">
+                    <a class="nav-link" style="font: size 25px;" href="#">FILM AGENDA</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ALLE VESTIGINGEN</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">CONTACT</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="purple-section">
+        <span class="ms-5fix">KOOP JE TICKETS</span>
+        <select class="background-color d-inline">
+            <option>Kies je film</option>
+        </select>
+        <button id="buy_tickets">BESTEL TICKETS</button>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
