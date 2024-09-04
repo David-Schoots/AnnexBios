7c7e53770bb64f148243ca7a5bce06fea019e867
David-Schoots <davidschoots@live.nl>
2024-09-04 13:46:20 +0200

    added footer

---
+++ page/footer.php
@@ -0,0 +1,44 @@
+<footer style="padding: 20px 0; position: relative; background: #707070; overflow: hidden;">
+  <div class="container">
+    <div class="row" style="position:relative;">
+      <!-- 1st Column: Logo and Text -->
+      <div class="col-12 col-md-5 mb-4 d-flex flex-column align-items-start">
+        <div class="row">
+          <div class="col-12">
+            <img src="./assets/logo/logo_footer.png" class="img-fluid mb-3" width="50%" alt="logo">
+          </div>
+          <div class="col-7">
+            <p class="mt-2" style="color: white;">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+            </p>
+          </div>
+          <div class="col-12">
+            <button style="background-color: white; color: black; border: none; padding: 10px 20px; margin-top: 10px;">LEES MEER</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- 2nd Column: Navigation Links -->
+      <div class="col-12 col-md-3 mb-4 mt-5 d-flex flex-column align-items-start">
+        <h3 style="color: white;">NAVIGEER</h3>
+        <a href="#" style="color: white;" class="d-block mb-2">Werken bij</a>
+        <a href="#" style="color: white;" class="d-block mb-2">Veelgestelde vragen</a>
+        <a href="#" style="color: white;" class="d-block mb-2">Vestigingen</a>
+        <a href="#" style="color: white;" class="d-block">Contact</a>
+      </div>
+
+      <!-- 3rd Column: Social Media -->
+      <div class="col-12 col-md-4 mb-4 mt-5 align-items-start">
+        <h3 style="color: white;">VOLG ONS</h3>
+        <div class="align-items-center">
+          <a href="#" title="Facebook"><img src="./assets/icons/facebook.png" width="10%" alt="Facebook"></a>
+          <a href="#" title="Twitter"><img src="./assets/icons/twitter.png"  width="10%" alt="Twitter"></a>
+          <a href="#" title="Instagram"><img src="./assets/icons/instagram.png" width="10%" alt="Instagram"></a>
+        </div>
+      </div>    
+    </div>
+
+    <!-- Filmstrip image positioned -->
+    <img src="./assets/icons/film.png" alt="film" class="position-absolute" style="height: 125%; right: -35px; bottom: -50px; transform: rotate(-180deg);">
+  </div>
+</footer>
