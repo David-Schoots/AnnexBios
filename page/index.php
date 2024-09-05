94c33ab1f479cd8e9c09dd8169ffdbfb7157bf56
David-Schoots <davidschoots@live.nl>
2024-09-05 11:30:44 +0200

    added the layout for the information

--- page/index.php
+++ page/index.php
@@ -12,19 +12,32 @@ include_once("header.php");
 
 <!-- information company -->
 
-<div class="container mt-4 p-4" style="background-color: #6E4F7D; max-width: 86%;">
-        <div class="row">
-            <!-- 1st column: photo -->
-            <div class="col-12 col-md-6 mb-4">
-                <img src="../assets/maps/maps.png" alt="location" class="img-fluid" style="max-height: 300px; width: 50%;">
-            </div>
-            
-            
+
+<div class="container my-5" style="background-color: #f1f1f1; max-width: 86%;">
+    <div class="row text-white">
+      <div class="col-md-6">
+        <div class="info-block mt-3">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2451.8740418078987!2d5.05273527620053!3d52.082022768564215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c665600a203a4b%3A0x50deab8a2f8797c8!2sRijksstraatweg%2042%2C%203454%20JC%20Utrecht!5e0!3m2!1snl!2snl!4v1725526494196!5m2!1snl!2snl" width="70%" height="200px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="p-2"style="background-color: #6E4F7D;">
+            <p class="mt-3">
+            <strong><i class="bi bi-geo-alt-fill text-white"></i></strong> Rijksstraatweg 42, 3232 KA Hellevoetsluis<br>
+            <strong><i class="bi bi-telephone-fill text-white"></i></strong> 020-12345678
+          </p>
+          <p>
+            <strong>Bereikbaarheid:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis.
+          </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6 mt-4 my-2">
+        <div class="theater-image">
+          <img src="../assets/maps/Hellevoetssluis.png" alt="Tivoli Theater" width="90%">
+        </div>
+      </div>
     </div>
+  </div>
 
-   
-
-</div> <!-- End of information company -->
 
 
 
