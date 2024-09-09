971bae40d41557d31e6bf1836ae34bfd857582d0
Tjardo <tjardo.antonie@gmail.com>
2024-09-09 09:53:01 +0200

    overview page

--- page/overview.php
+++ page/overview.php
@@ -1,46 +1,7 @@
 <?php
 include_once("header.php");
 include_once("movieloop.php");
-
 ?>
-
-<!-- Introduction Section -->
-<div class="container mt-5 text-white p-4" style="background-color: #6E4F7D;">
-    <h1 class="mb-4 text-uppercase" style="font-size: 60px;">welkom bij annexbios 3</h1>
-    <p class="mb-4" style="font-size: 25px; max-width: 850px;">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient</p>
-    <button class="px-4 py-2 fw-bold" style="color: #6E4F7D; border: none;">BEKIJK DE DRAAIENDE FILMS</button>
-</div>
-<!-- End of Introduction Section -->
-
-<!-- Information Company -->
-<!-- Information Company -->
-<div class="container my-5" style="background-color: white;">
-    <div class="row text-dark align-items-stretch">
-        <div class="col-md-6 d-flex flex-column">
-            <div class="info-block mt-3 flex-grow-1">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2451.8740418078987!2d5.05273527620053!3d52.082022768564215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c665600a203a4b%3A0x50deab8a2f8797c8!2sRijksstraatweg%2042%2C%203454%20JC%20Utrecht!5e0!3m2!1snl!2snl!4v1725526494196!5m2!1snl!2snl" width="100%" height="200px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                <div class="px-2" style="background-color: #6E4F7D; color: white;">
-                    <p class="mt-3 py-2">
-                        <strong><i class="bi bi-geo-alt-fill"></i></strong> Rijksstraatweg 42, 3232 KA Hellevoetsluis<br>
-                        <strong><i class="bi bi-telephone-fill"></i></strong> 020-12345678
-                    </p>
-                    <strong>Bereikbaarheid</strong>
-                    <p style="max-width: 550px; padding-bottom: 10px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-6 d-flex align-items-stretch">
-            <div class="theater-image d-flex align-items-center justify-content-center" style="padding: 15px;">
-                <img src="../assets/maps/Hellevoetssluis.png" alt="Tivoli Theater" class="img-fluid" style="max-height: 100%; object-fit: cover;">
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
 
 <!-- Movie Section -->
 <div class="container my-5 text-uppercase" style=" font-size: 45px;">
@@ -111,18 +72,22 @@ include_once("movieloop.php");
     </div>
 </div>
 
-<!-- movie posters -->
+<!-- Movie Posters -->
 <div class="container my-5">
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 g-3">
-        <?php foreach ($array as $movie): ?>
-            <?php
+        <?php
+        $totalMovies = 18;  // We willen in totaal 18 films tonen
+        $movieCount = count($array);  // Aantal films in je array
+        for ($i = 0; $i < $totalMovies; $i++):
+            // Gebruik modulo om door de array te lopen en films te herhalen
+            $movie = $array[$i % $movieCount];
+
             // Encode movie data
             $encodedMovie = base64_encode(json_encode($movie));
-            ?>
+        ?>
             <div class="col">
                 <div class="card d-flex flex-column justify-content-between h-100" style="border: none;">
-                    <img src="../<?php echo $movie['photo']['photo1']; ?>" class="card-img-top img-fluid" style="min-height: 350px; object-fit: cover; width: 100%;"
-                        alt="<?php echo $movie['title']; ?>" />
+                    <img src="../<?php echo $movie['photo']['photo1']; ?>" class="card-img-top img-fluid" style="min-height: 350px; object-fit: cover; width: 100%;" alt="<?php echo $movie['title']; ?>" />
                     <div class="card-body d-flex flex-column">
                         <h5 class="card-title text-uppercase"><?php echo $movie['title']; ?></h5>
                         <span style="color: #6E4F7D; font-size: 1.5rem;"><?php echo $movie['stars']; ?></span>
@@ -134,17 +99,17 @@ include_once("movieloop.php");
                     </div>
                 </div>
             </div>
-        <?php endforeach; ?>
+        <?php endfor; ?>
     </div>
 </div>
+
+
+
 
 <!-- button to the all movie page -->
 <div class="container my-5">
     <a href="#" class="btn btn-primary text-uppercase" style="background-color: #6E4F7D;border:none; min-width:15%">Bekijk Alle Films</a>
 </div>
-
-
-
 
 <?php
 include_once("footer.php");
---
+++ page/overviewloop.php
@@ -0,0 +1,45 @@
+<?php
+$array =
+    [
+        [
+            "id" => 1,
+            "title" => "JURASSIC WORLD: FALLEN KINGDOM",
+            "description" => "Welkom in Jurassic World: Fallen Kingdom! Favoriete personages keren terug in dit 3D actie-spektakel.",
+            "stars" => "★★★★☆",
+            "release" => "7-06-2018",
+            "photo" => [
+                "photo1" => "assets/films/Jurassic-World_-Fallen-Kingdom.jpg"
+            ]
+        ],
+        [
+            "id" => 2,
+            "title" => "Deadpool 2",
+            "description" => "Na het overleven van een bijna fatale runderaanval, worstelt een misvormde cafetaria-kok (Wade Wilson) om zijn droom",
+            "stars" => "★★★★☆",
+            "release" => "17-05-2018",
+            "photo" => [
+                "photo1" => "assets/films/deadpool.jpg"
+            ]
+        ],
+        [
+            "id" => 3,
+            "title" => "SOLO: A STAR WARS STORY",
+            "description" => "Een compleet nieuw avontuur uit een ‘galaxy far, far away’, dat het verhaal vertelt over het verleden van de iconische smokkelaar,",
+            "stars" => "★★★☆☆",
+            "release" => "23-05-2018",
+            "photo" => [
+                "photo1" => "assets/films/solo.jpeg"
+            ]
+        ],
+        [
+            "id" => 4,
+            "title" => "Pieter konijn",
+            "description" => "Verfilming van Beatrix Potter’s tijdloze verhaal over een eigenwijs konijn dat probeert de moestuin van een boer binnen te dringen.",
+            "stars" => "★★★★☆",
+            "release" => "28-03-2018",
+            "photo" => [
+                "photo1" => "assets/films/pieter-konijn.jpg"
+            ]
+        ],
+
+    ];
