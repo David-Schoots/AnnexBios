5f09b4c1818de6720d814243bfe6600680fba781
David-Schoots <davidschoots@live.nl>
2024-09-10 12:37:48 +0200

    added time and date selector

--- page/buyticket.php
+++ page/buyticket.php
@@ -1,24 +1,40 @@
-<?php include_once("header.php");
-
+<?php 
+  include_once("header.php");
 ?>
 
-<div class="container my-5 text-uppercase" style=" font-size: 45px;">
+<div class="container my-5 text-uppercase" style="font-size: 45px;">
   <div class="row">
     <div class="col-6 bg-white">
-    <h2 class="text-left" style="color: #6E4F7D;">TICKETS BESTELLEN</h2>
+      <h2 class="text-left" style="color: #6E4F7D;">TICKETS BESTELLEN</h2>
     </div>
   </div>
 </div>
 
-
 <div class="container my-5 d-flex">
     <div class="col-12 d-flex flex-row gap-3">
         <p class="p-1" style="background-color: white;"><?= $movie['title'] ?></p>
-        <p class="p-1" style="background-color: white;" >Datum</p>
-        <p class="p-1" style="background-color: white;" >Tijdstip</p>
+
+        <!-- Datepicker Dropdown -->
+        <div class="dropdown">
+          <button class="btn btn-light dropdown-toggle" style="color: #6E4F7D;" type="button" id="dropdownDateButton" data-bs-toggle="dropdown" aria-expanded="false">
+            Datum
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownDateButton">
+            <li>
+              <input type="text" class="form-control datepicker" placeholder="Kies een datum">
+            </li>
+          </ul>
+        </div>
+
+          <div class="dropdown">
+            <button class="btn btn-light dropdown-toggle" style="color: #6E4F7D;" type="button" id="dropdownTimeButton" data-bs-toggle="dropdown" aria-expanded="false">
+              Tijdstip
+            </button>
+            <div class="dropdown-menu p-3" aria-labelledby="dropdownTimeButton">
+              <input type="text" class="form-control test">
+            </div>
+          </div>
     </div>
 </div>
 
-
-
-<?php include_once("footer.php");?>
+<?php include_once("footer.php"); ?>
--- page/header.php
+++ page/header.php
@@ -30,14 +30,17 @@ if (isset($_GET['id'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AnnexBios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../css/header.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+    <!-- datepicker css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
+    <!-- Timepicker css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.css" integrity="sha512-4S7w9W6/qX2AhdMAAJ+jYF/XifUfFtrnFSMKHzFWbkE2Sgvbn5EhGIR9w4tvk0vfS1hKppFIbWt/vdVIFrIAKw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
-    <div class=" fixed-top">
+    <div class="fixed-top">
         <nav class="navbar navbar-expand-md navbar-light bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="../page/index.php">
@@ -62,7 +65,6 @@ if (isset($_GET['id'])) {
             </div>
         </nav>
 
-
         <div class="purple-section d-flex flex-column flex-md-row align-items-center text-center text-md-start" style="background-color: #6e4778; padding: 20px; color: white; font-weight: bold;">
             <span class="mb-2 mb-md-0" style="margin-left: 5%;">KOOP JE TICKETS</span>
             <select class="text-white fw-bold mt-2 mt-md-0 ms-md-3" style="background-color: #a386b1; height: 5vh; width: 230px; font-size: 15px;">
@@ -73,10 +75,20 @@ if (isset($_GET['id'])) {
     </div>
 
 
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <!-- jquery js -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
     <script src="../js/overide.js"></script>
+    <!-- script for the datepicker js -->
+    <script src="../js/datepicker.js"></script>
+    <script src="../js/timepicker.js"></script>
+    <!-- script for the timepicker.js -->
+    <<script src="https://cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.js" integrity="sha512-ux1VHIyaPxawuad8d1wr1i9l4mTwukRq5B3s8G3nEmdENnKF5wKfOV6MEUH0k/rNT4mFr/yL+ozoDiwhUQekTg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+
+    
 
 </body>
 
