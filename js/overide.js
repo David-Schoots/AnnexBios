41b4ae86ea5e70ccdd9289bba1d9dd2fc7fea4b7
Tjardo <tjardo.antonie@gmail.com>
2024-09-11 15:09:28 +0200

    dropdown

--- js/overide.js
+++ js/overide.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
     extraButtons.classList.toggle("d-none");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".dropdown-item").forEach(function (item) {
+    item.addEventListener("click", function (event) {
+      event.preventDefault();
+      let selectedValue = event.target.getAttribute("data-value");
+      let dropdownButton = event.target
+        .closest(".dropdown")
+        .querySelector(".btn");
+      dropdownButton.textContent = selectedValue;
+    });
+  });
+});
